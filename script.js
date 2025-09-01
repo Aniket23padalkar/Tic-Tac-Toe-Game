cd9b8b92ef81ffcbc4ab0d97e59adeff7a58641e
Aniket Padalkar <aniket23.padalkar@gamil.com>
2025-09-01 20:08:29 +0530

    created winning combos

--- script.js
+++ script.js
@@ -4,20 +4,36 @@ const winLose = document.getElementById("win-lose");
 
 let turnX = true;
 
+let turns = ["", "", "", "", "", "", "", "", ""];
+
+let winningCombo = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
 cellBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
-        const index = btn.dataset.index;
+        const index = Number(btn.dataset.index);
 
         if (btn.textContent === "") {
             if (turnX) {
                 btn.textContent = "X";
                 turnX = false;
+                turns[index] = "X";
                 winLose.textContent = "Player 'O' Turn";
             } else {
                 btn.textContent = "O";
                 turnX = true;
+                turns[index] = "O";
                 winLose.textContent = "Player 'X' Turn";
             }
+            console.log(turns);
         }
     });
 });
@@ -28,4 +44,5 @@ restart.addEventListener("click", () => {
     });
     turnX = true;
     winLose.textContent = "Player 'X' Turn";
+    turns = ["", "", "", "", "", "", "", "", ""];
 });
